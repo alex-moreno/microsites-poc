d1285387d77cb528d7491e3b1933173edae191b7
Alex Moreno-Lopez <alejandro.moreno@tdo.es>
2022-07-01 13:47:24 +0200

    minor changes.

--- web/private/scripts/new_site.php
+++ web/private/scripts/new_site.php
@@ -1,27 +1,11 @@
 <?php
-
-echo "Quicksilver Debuging Output: CREATING NEW SITE ";
-echo "\n\n";
-echo "\n========= START PAYLOAD ===========\n";
-print_r($_POST);
-echo "\n========== END PAYLOAD ============\n";
-
-echo "\n------- START ENVIRONMENT ---------\n";
-$env = $_ENV;
-foreach ($env as $key => $value) {
-  if (preg_match('#(PASSWORD|SALT|AUTH|SECURE|NONCE|LOGGED_IN)#', $key)) {
-    $env[$key] = '[REDACTED]';
-  }
-}
-print_r($env);
-echo "\n-------- END ENVIRONMENT ----------\n";
-
-print("\n==== Drupal Environment Initialization Starting ====\n");
+print("\n==== Drupal Environment importing database ====\n");
 // Get paths for imports
 $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
 
 // Import database
 echo ('Importing Database from ...');
-echo "${path}/database.sql && drush cr";
-$cmd = "drush sql:cli < ${path}/database.sql && drush cr";
-//passthru($cmd);
+echo "${path}/microsite-database.sql && drush cr";
+// Please don't store your database in the repository. This is just for demo purposes.
+$cmd = "drush sql:cli < ${path}/microsite-database.sql && drush cr";
+passthru($cmd);
--- web/private/scripts/sync.php
+++ web/private/scripts/sync.php
@@ -1,5 +1,21 @@
 <?php
 
+echo "Quicksilver Debuging Output: SYNC ";
+echo "\n\n";
+echo "\n========= START PAYLOAD ===========\n";
+ print_r($_POST);
+echo "\n========== END PAYLOAD ============\n";
+
+echo "\n------- START ENVIRONMENT ---------\n";
+ $env = $_ENV;
+ foreach ($env as $key => $value) {
+   if (preg_match('#(PASSWORD|SALT|AUTH|SECURE|NONCE|LOGGED_IN)#', $key)) {
+     $env[$key] = '[REDACTED]';
+   }
+ }
+print_r($env);
+echo "\n-------- END ENVIRONMENT ----------\n";
+
 print("\n==== Drupal Environment importing database ====\n");
 // Get paths for imports
 $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
