15e97e4d63f36a618fee718af1aa9be467dd7f02
Alex Moreno-Lopez <alejandro.moreno@tdo.es>
2022-07-01 11:40:23 +0200

    removed protected folders.

--- private/scripts/sync.php
+++ private/scripts/sync.php
@@ -3,8 +3,10 @@
 echo "Quicksilver Debuging Output: SYNC ";
 echo "\n\n";
 echo "\n========= START PAYLOAD ===========\n";
+// print_r($_POST);
 echo "\n========== END PAYLOAD ============\n";
 
 echo "\n------- START ENVIRONMENT ---------\n";
 
 echo "\n-------- END ENVIRONMENT ----------\n";
+
