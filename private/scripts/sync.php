a83b8298ec21b31f7bca9aedade38a87572731d8
Alex Moreno-Lopez <alejandro.moreno@tdo.es>
2022-07-01 10:22:43 +0200

    Moved folders.

--- private/scripts/sync.php
+++ private/scripts/sync.php
@@ -3,7 +3,7 @@
 echo "Quicksilver Debuging Output: SYNC ";
 echo "\n\n";
 echo "\n========= START PAYLOAD ===========\n";
-print_r($_POST);
+// print_r($_POST);
 echo "\n========== END PAYLOAD ============\n";
 
 echo "\n------- START ENVIRONMENT ---------\n";
